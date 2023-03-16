c3e1a56b08e71a7427ecd7911983b83787b5a835
Студент <@CAB31-14-PC>
2023-03-16 12:06:02 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp154/ConsoleApp154/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = { 8, -1, 4, 16, -5, 3 };
+int max = array[0];int indexmax = 0;
+for(int i=0;i<array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i]; indexmax = i;
+    }
+}
+Console.WriteLine($"max={max} indexmax={indexmax}");
